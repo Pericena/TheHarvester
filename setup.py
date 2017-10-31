ab92a82c065eabb95170b7b4b24c132a9e65279a
Luishiño Pericena Choque <pericena12@gmail.com>
2017-10-31 17:32:40 -0400

    Add files via upload
    Información:
    Blogger          https://lpericena.blogspot.com/
    Github            https://github.com/Pericena
    Grupo            https://www.facebook.com/groups/122223121705126/?ref=bookmarks
    pinterest        https://es.pinterest.com/lushiopericena/
    twitter             https://twitter.com/LPericena
    linkedin          https://www.linkedin.com/in/luishi%C3%B1o-pericena-choque-126534116/
    facebook       https://www.facebook.com/profile.php?id=100009309755063
    pagina facebook  https://www.facebook.com/lpericena
    sitio web        https://pericena.wordpress.com/
    Aplicacion     https://apps.facebook.com/167466933725219
    contacto        https://www.facebook.com/Servicio-T%C3%A9cnico-1506435219407506/
    https://www.socialtools.me/index?action=demoApps&preview=1&app_id=406101
    ask      https://ask.fm/Lpericena
    soundcloud   https://soundcloud.com/luishino-pericena-choque

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#powerhell python setup.py py2exe
+
+import sys
+from distutils.core import setup
+
+kwargs = {}
+if 'py2exe' in sys.argv:
+    import py2exe
+    kwargs = {
+        'console' : [{
+            'script'         : 'theHarvester.py',
+            'description'    : 'Descripcion del programa.',
+           'icon_resources' : [(0, 'icon.ico')]
+            }],
+        'zipfile' : None,
+        'options' : { 'py2exe' : {
+            'dll_excludes'   : ['w9xpopen.exe'],
+            'bundle_files'   : 1,
+            'compressed'     : True,
+            'optimize'       : 2
+            }},
+         }
+
+setup(
+    name='theHarvester',
+    author='Luishiño Pericena Choque',
+    author_email='pericena12@gmail.com',
+    **kwargs)
